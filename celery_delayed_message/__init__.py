bdd1a8e733309ea4598d865294530b45ae02bea9
yubo <yubo-jk@360jinrong.net>
2021-03-28 19:18:11 +0800

    Fixed & test delayed message to ensure it's OK to work

--- celery_delayed_message/__init__.py
+++ celery_delayed_message/__init__.py
@@ -1,9 +1,30 @@
 from celery.app import task
+from celery.utils.log import get_logger
 
 from .tasks import DelayTask
 
-__all__ = ["DelayTask", "patch_celery_task"]
+__all__ = ["DelayTask", "monkey"]
+
+logger = get_logger(__name__)
 
 
 def patch_celery_task():
+
     task.Task = DelayTask
+    logger.info('celery delayed message patched successfully.')
+
+
+def patch(app):
+    patch_celery_task()
+
+    # Register `requeue_manager` task
+    from .redis_handlers import faker as requeue_faker
+    from .redis_handlers import manager as requeue_manager
+    from .redis_handlers import get_requeue_task
+    requeue_faker.app = app
+    requeue_manager.app = app
+    # app.tasks.register(requeue_faker)
+    app.tasks.register(get_requeue_task(app))
+
+
+monkey = type('Monkey', (object, ), dict(patch=patch))
